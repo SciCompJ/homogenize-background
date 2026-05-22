454cb2af673584339e5c324c20c671f7908176b9
dlegland <david.legland@inrae.fr>
2026-05-22 15:32:38 +0200

    add support for RGB8 images

--- src/main/java/imago/plugin/vignetting/FitBackgroundPlugin.java
+++ src/main/java/imago/plugin/vignetting/FitBackgroundPlugin.java
@@ -15,6 +15,9 @@ import net.sci.array.Arrays;
 import net.sci.array.binary.Binary;
 import net.sci.array.binary.BinaryArray;
 import net.sci.array.binary.BinaryArray2D;
+import net.sci.array.color.RGB8;
+import net.sci.array.color.RGB8Array;
+import net.sci.array.color.RGB8Array2D;
 import net.sci.array.numeric.Float32Array2D;
 import net.sci.array.numeric.Scalar;
 import net.sci.array.numeric.ScalarArray;
@@ -86,9 +89,9 @@ public class FitBackgroundPlugin implements ImageFramePlugin
             ImagoGui.showErrorDialog(frame, "Both images must have same size", "Dimension Error");
             return;
         }
-        if (!Scalar.class.isAssignableFrom(array.elementClass()))
+        if (!Scalar.class.isAssignableFrom(array.elementClass()) && array.elementClass() != RGB8.class)
         {
-            ImagoGui.showErrorDialog(frame, "Reference image must contain scalar data\nCurrent class is: " + array.elementClass().getName(), "Image Type Error");
+            ImagoGui.showErrorDialog(frame, "Reference image must contain scalar or RGB8 data\nCurrent class is: " + array.elementClass().getName(), "Image Type Error");
             return;
         }
         if (mask.elementClass() != Binary.class)
@@ -97,27 +100,46 @@ public class FitBackgroundPlugin implements ImageFramePlugin
             return;
         }
         
+        if (showResiduals && !Scalar.class.isAssignableFrom(array.elementClass()))
+        {
+            ImagoGui.showErrorDialog(frame, "Can not compute residuals of non scalar images", "Image Type Error");
+            return;
+        }
+        
         frame.algoStatusChanged(new AlgoEvent(this, "Start Vignetting removal"));
         BinaryArray2D bgMask = BinaryArray2D.wrap((BinaryArray.wrap(mask)).complement());
         
+        Array<?> bgFit;
         frame.algoStatusChanged(new AlgoEvent(this, "Start Background normalization"));
-        @SuppressWarnings({ "unchecked", "rawtypes" })
-        ScalarArray2D<?> array2d = ScalarArray2D.wrapScalar2d(ScalarArray.wrap((Array<? extends Scalar>) array));
-        ScalarArray2D<?> bgFit = fitBackground(array2d, bgMask, maxOrder, samplingStep);
+        if (Scalar.class.isAssignableFrom(array.elementClass()))
+        {
+            @SuppressWarnings({ "unchecked", "rawtypes" })
+            ScalarArray2D<?> array2d = ScalarArray2D.wrapScalar2d(ScalarArray.wrap((Array<? extends Scalar>) array));
+            bgFit = fitBackground(array2d, bgMask, maxOrder, samplingStep);
 
+            if (showResiduals)
+            {
+                ScalarArray2D<?> resid = residuals((ScalarArray2D<?>) bgFit, array2d, bgMask);
+                Image residImage = new Image(resid, ImageType.DIVERGING, refImage);
+                residImage.setName(refImage.getName() + "-bgFit-resid");
+                double[] valueRange = resid.finiteValueRange();
+                double maxAbsDiff = Math.max(valueRange[0], valueRange[1]);
+                residImage.getDisplaySettings().setDisplayRange(new double[] {-maxAbsDiff, maxAbsDiff});
+                ImageFrame.create(residImage, frame);
+            }
+        }
+        else if (array.elementClass() == RGB8.class)
+        {
+            RGB8Array2D array2d = RGB8Array2D.wrap(RGB8Array.wrap(array));
+            bgFit = fitBackground_rgb(array2d, bgMask, maxOrder, samplingStep);
+        }
+        else
+        {
+            throw new RuntimeException("Can not process array with element class: " + array.elementClass().getName());
+        }
+        
         Image result = new Image(bgFit, refImage);
         result.setName(refImage.getName() + "-bgFit");
-
-        if (showResiduals)
-        {
-            ScalarArray2D<?> resid = residuals(bgFit, array2d, bgMask);
-            Image residImage = new Image(resid, ImageType.DIVERGING, refImage);
-            residImage.setName(refImage.getName() + "-bgFit-resid");
-            double[] valueRange = resid.finiteValueRange();
-            double maxAbsDiff = Math.max(valueRange[0], valueRange[1]);
-            residImage.getDisplaySettings().setDisplayRange(new double[] {-maxAbsDiff, maxAbsDiff});
-            ImageFrame.create(residImage, frame);
-        }
         
         // add the image document to GUI
         ImageFrame.create(result, frame);
@@ -135,6 +157,19 @@ public class FitBackgroundPlugin implements ImageFramePlugin
         FitPolynomialBackground pbg = new FitPolynomialBackground(orderMax);
         pbg.setSamplingStep(samplingStep);
         return pbg.process(image, mask);
+    }
+    
+    public static final RGB8Array2D fitBackground_rgb(RGB8Array2D image, BinaryArray2D mask,
+            int orderMax, int samplingStep)
+    {
+        FitPolynomialBackground pbg = new FitPolynomialBackground(orderMax);
+        pbg.setSamplingStep(samplingStep);
+        RGB8Array2D res = RGB8Array2D.create(image.size(0), image.size(1));
+        for (int c = 0; c < 3; c++)
+        {
+            res.setChannel(c, pbg.process(image.channel(c), mask));
+        }
+        return res;
     }
     
     private static final ScalarArray2D<?> residuals(ScalarArray2D<?> fitted, ScalarArray2D<?> image, BinaryArray2D mask)
